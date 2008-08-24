985aa91a2418808e26bbfb9375c1c7de0f6a8420
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2008-08-24 18:06:04 +0000

    - More GLSL related work. Added framework for a render state that is maintained outside OpenGL. - Re-fixed my decal lighting fix from last month.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@165 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/glsl_state.h
+++ src/gl/glsl_state.h
@@ -13,15 +13,23 @@ public:
 
 	void SetFixedColormap(int cm);
 	void SetLight(int light, int rellight, FColormap *cm, float alpha, bool additive);
-	void SetLight(int light, int rellight, FColormap *cm, float alpha, bool additive, PalEntry ThingColor, bool weapon);
+	void SetLight(int light, int rellight, FColormap *cm, float alpha, bool additive, PalEntry ThingColor);
+	void SetAddLight(float *f);
+	void SetLightAbsolute(float r, float g, float b, float a);
+	void EnableFog(bool on);
 	void SetWarp(int mode, float speed);
+	void EnableTexture(bool on);
 	void EnableBrightmap(bool on);
+	void EnableFogBoundary(bool on);
 	void SetBrightmap(bool on);
 	void SetFloorGlow(PalEntry color, float height);
 	void SetCeilingGlow(PalEntry color, float height);
 	void ClearDynLights();
 	void SetDynLight(int index, float x, float y, float z, float size, PalEntry color, int mode);
 	void SetLightingMode(int mode);
+	void SetAlphaThreshold(float thresh);
+	void SetBlend(int src, int dst);
+	void SetTextureMode(int mode);
 	void Apply();
 	void Enable(bool on);
 	void Reset();
