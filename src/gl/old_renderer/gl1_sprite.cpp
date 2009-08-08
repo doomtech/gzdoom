13d723af33acd72b01eed97b37594db38ba6b848
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2009-08-08 20:54:45 +0000

    - fixed XY billboarding,
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@415 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/old_renderer/gl1_sprite.cpp
+++ src/gl/old_renderer/gl1_sprite.cpp
@@ -217,7 +217,7 @@ void GLSprite::Draw(int pass)
 			float xcenter = (x1+x2)*0.5;
 			float ycenter = (y1+y2)*0.5;
 			float zcenter = (z1+z2)*0.5;
-			float angleRad = FLOAT_TO_RAD(GLRenderer->mAngles.Yaw);
+			float angleRad = FLOAT_TO_RAD(270. - GLRenderer->mAngles.Yaw);
 			
 			Matrix3x4 mat;
 			mat.MakeIdentity();
