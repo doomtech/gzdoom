14e4f6c0e79cd5bb8a3969fdcd5e083ea6f329bf
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2009-10-29 06:57:21 +0000

    - took #version directive out of the shader source. It now gets prepended by the compiling code   because ATI does not accept it inside #ifdefs.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@584 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/shaders/gl_shader.cpp
+++ src/gl/shaders/gl_shader.cpp
@@ -270,7 +270,12 @@ FShaderContainer::FShaderContainer(const char *ShaderName, const char *ShaderPat
 			try
 			{
 				FString str = shaderdefines[i];
-				if (i>3 && gl.MaxLights() == 128) str << "#define MAXLIGHTS128\n";
+				if (i>3)
+				{
+					// this can't be in the shader code due to ATI strangeness.
+					str << "#version 120\n#extension GL_EXT_gpu_shader4 : enable\n";
+					if (gl.MaxLights() == 128) str << "#define MAXLIGHTS128\n";
+				}
 				shader[i] = new FShader;
 				if (!shader[i]->Load(name, "shaders/glsl/main.vp", "shaders/glsl/main.fp", ShaderPath, str.GetChars()))
 				{
