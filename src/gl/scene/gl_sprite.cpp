3c145f04f3ec6ea7e21e658c8b0f8cccedad25bf
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2010-01-16 08:04:42 +0000

    - fixed: Sprite with an alpha component must always be drawn in translucent mode.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@718 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/scene/gl_sprite.cpp
+++ src/gl/scene/gl_sprite.cpp
@@ -706,6 +706,8 @@ void GLSprite::Process(AActor* thing,sector_t * sector)
 	if ((gltexture && gltexture->GetTransparent()) || (RenderStyle.Flags & STYLEF_RedIsAlpha))
 	{
 		hw_styleflags = STYLEHW_NoAlphaTest;
+		RenderStyle.SrcAlpha = STYLEALPHA_Src;
+		RenderStyle.DestAlpha = STYLEALPHA_InvSrc;
 	}
 
 	if (enhancedvision && gl_enhanced_nightvision)
