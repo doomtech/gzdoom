dd29e02beb7733f9e83c99c74dd41947ca6cedbf
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2010-09-23 09:50:20 +0000

    - version bump
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@990 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/version.h
+++ src/version.h
@@ -41,15 +41,15 @@
 
 /** Lots of different version numbers **/
 
-#define DOTVERSIONSTR_NOREV "1.5.2"
+#define DOTVERSIONSTR_NOREV "1.5.3"
 #define ZDVER_STRING "2.5.0"
 
 // The version string the user actually sees.
 #define DOTVERSIONSTR DOTVERSIONSTR_NOREV " (r" SVN_REVISION_STRING ") / ZDoom " ZDVER_STRING " (r" ZD_SVN_REVISION_STRING ")"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 1,5,2,SVN_REVISION_NUMBER
-#define RC_PRODUCTVERSION 1,5,2,0
+#define RC_FILEVERSION 1,5,3,SVN_REVISION_NUMBER
+#define RC_PRODUCTVERSION 1,5,3,0
 #define RC_FILEVERSION2 DOTVERSIONSTR
 #define RC_PRODUCTVERSION2 "1.5"
 
