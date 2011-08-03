945ede9f5a23a0aa87881083afa62617faa60ffb
gez <gez@b0f79afe-0144-0410-b225-9a4edf0717df>
2011-08-03 00:29:00 +0000

    - Made particle colors palette-independent in OpenGL.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1234 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/scene/gl_sprite.cpp
+++ src/gl/scene/gl_sprite.cpp
@@ -897,7 +897,7 @@ void GLSprite::ProcessParticle (particle_t *particle, sector_t *sector)//, int s
 	RenderStyle = STYLE_Translucent;
 	OverrideShader = 0;
 
-	ThingColor = GPalette.BaseColors[particle->color];
+	ThingColor = particle->color; //GPalette.BaseColors[particle->color];
 	ThingColor.a=0;
 
 	modelframe=NULL;
