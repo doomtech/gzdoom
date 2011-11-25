3940936e669c0c9724d73751d4cea9fa040446ae
gez <gez@b0f79afe-0144-0410-b225-9a4edf0717df>
2011-11-25 15:59:12 +0000

    - Fixed: the NOTELEPORT flag is removed from Dehacked missiles which lose the MISSILE flag. This caused problems with certain special effects based on dehacked spawn cubes. - Fixed: all Boom silent teleporters preserve relative height.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1266 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/p_teleport.cpp
+++ src/p_teleport.cpp
@@ -99,7 +99,7 @@ void P_SpawnTeleportFog(fixed_t x, fixed_t y, fixed_t z, int spawnid)
 //
 
 bool P_Teleport (AActor *thing, fixed_t x, fixed_t y, fixed_t z, angle_t angle,
-				 bool useFog, bool sourceFog, bool keepOrientation, bool bHaltVelocity)
+				 bool useFog, bool sourceFog, bool keepOrientation, bool bHaltVelocity, bool keepHeight)
 {
 	fixed_t oldx;
 	fixed_t oldy;
@@ -122,7 +122,11 @@ bool P_Teleport (AActor *thing, fixed_t x, fixed_t y, fixed_t z, angle_t angle,
 		player = NULL;
 	floorheight = destsect->floorplane.ZatPoint (x, y);
 	ceilingheight = destsect->ceilingplane.ZatPoint (x, y);
-	if (z == ONFLOORZ)
+	if (keepHeight)
+	{
+		z = floorheight + aboveFloor;
+	}
+	else if (z == ONFLOORZ)
 	{
 		if (player)
 		{
@@ -315,7 +319,7 @@ static AActor *SelectTeleDest (int tid, int tag)
 }
 
 bool EV_Teleport (int tid, int tag, line_t *line, int side, AActor *thing, bool fog,
-				  bool sourceFog, bool keepOrientation, bool haltVelocity)
+				  bool sourceFog, bool keepOrientation, bool haltVelocity, bool keepHeight)
 {
 	AActor *searcher;
 	fixed_t z;
@@ -372,7 +376,7 @@ bool EV_Teleport (int tid, int tag, line_t *line, int side, AActor *thing, bool 
 	{
 		badangle = 1 << ANGLETOFINESHIFT;
 	}
-	if (P_Teleport (thing, searcher->x, searcher->y, z, searcher->angle + badangle, fog, sourceFog, keepOrientation, haltVelocity))
+	if (P_Teleport (thing, searcher->x, searcher->y, z, searcher->angle + badangle, fog, sourceFog, keepOrientation, haltVelocity, keepHeight))
 	{
 		// [RH] Lee Killough's changes for silent teleporters from BOOM
 		if (!fog && line && keepOrientation)
