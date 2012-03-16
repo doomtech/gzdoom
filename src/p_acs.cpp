9d4d2f4d49ec39cbcf729f247ace03e35d160ab8
gez <gez@b0f79afe-0144-0410-b225-9a4edf0717df>
2012-03-16 00:22:53 +0000

    * Updated to ZDoom r3442: - Added clearlinespecial, for doom.wad e3m4 line 1069. - Allow using the parameterized compatibility options with IWAD maps. - Fixed: Destroying a weapon that claimed it was its own sister would crash. - Apply fix from previous commit to Doom-format maps. (Previous commit only did it for Hexen.) - Fixed: In non-UDMF maps, projectile impact activation implies projectile cross activation as well. - Applied a modified version of FDARI's patch to prevent giving health to dead things:   * P_GiveBody() now takes a max parameter so that it can also do the bulk of the work AHealth::TryPickup() previously did.   * Setting an actor's health to 0 or below with SetActorProperty will now kill the actor properly.- Added setactivation command for compatibility.cpp to fix the gear boxes on hexdd.wad, map54, which should be set for player uses activation but are not. - Added some developer mode messages for setlinespecial and clearlinespecial. - Fixed: Old-style ACS has the string table record offsets from the start of the object file to the strings, not from the start of the string table, so UnescapeStringTable() needs to be told where the offsets are based.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1313 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -1169,7 +1169,7 @@ FBehavior::FBehavior (int lumpnum, FileReader * fr, int len)
 	{
 		StringTable = LittleLong(((DWORD *)Data)[1]);
 		StringTable += LittleLong(((DWORD *)(Data + StringTable))[0]) * 12 + 4;
-		UnescapeStringTable(Data + StringTable, false);
+		UnescapeStringTable(Data + StringTable, Data, false);
 	}
 	else
 	{
@@ -1178,7 +1178,7 @@ FBehavior::FBehavior (int lumpnum, FileReader * fr, int len)
 		if (strings != NULL)
 		{
 			StringTable = DWORD(strings - Data + 8);
-			UnescapeStringTable(strings + 8, true);
+			UnescapeStringTable(strings + 8, NULL, true);
 		}
 		else
 		{
@@ -1621,7 +1621,7 @@ void FBehavior::LoadScriptsDirectory ()
 	scripts.b = FindChunk(MAKE_ID('S','N','A','M'));
 	if (scripts.dw != NULL)
 	{
-		UnescapeStringTable(scripts.b + 8, false);
+		UnescapeStringTable(scripts.b + 8, NULL, false);
 		for (i = 0; i < NumScripts; ++i)
 		{
 			// ACC stores script names as an index into the SNAM chunk, with the first index as
@@ -1688,15 +1688,23 @@ void FBehavior::UnencryptStrings ()
 // FBehavior :: UnescapeStringTable
 //
 // Processes escape sequences for every string in a string table.
+// Chunkstart points to the string table. Datastart points to the base address
+// for offsets in the string table; if NULL, it will use chunkstart. If
+// has_padding is true, then this is a STRL chunk with four bytes of padding
+// on either side of the string count.
 //
 //============================================================================
 
-void FBehavior::UnescapeStringTable(BYTE *chunkstart, bool has_padding)
+void FBehavior::UnescapeStringTable(BYTE *chunkstart, BYTE *datastart, bool has_padding)
 {
 	assert(chunkstart != NULL);
 
 	DWORD *chunk = (DWORD *)chunkstart;
 
+	if (datastart == NULL)
+	{
+		datastart = chunkstart;
+	}
 	if (!has_padding)
 	{
 		chunk[0] = LittleLong(chunk[0]);
@@ -1704,7 +1712,7 @@ void FBehavior::UnescapeStringTable(BYTE *chunkstart, bool has_padding)
 		{
 			int ofs = LittleLong(chunk[1 + strnum]);	// Byte swap offset, if needed.
 			chunk[1 + strnum] = ofs;
-			strbin((char *)chunk + ofs);
+			strbin((char *)datastart + ofs);
 		}
 	}
 	else
@@ -1714,7 +1722,7 @@ void FBehavior::UnescapeStringTable(BYTE *chunkstart, bool has_padding)
 		{
 			int ofs = LittleLong(chunk[3 + strnum]);	// Byte swap offset, if needed.
 			chunk[3 + strnum] = ofs;
-			strbin((char *)chunk + ofs);
+			strbin((char *)datastart + ofs);
 		}
 	}
 }
@@ -2866,10 +2874,20 @@ void DLevelScript::DoSetActorProperty (AActor *actor, int property, int value)
 	switch (property)
 	{
 	case APROP_Health:
+		// Don't alter the health of dead things.
+		if (actor->health <= 0 || (actor->player != NULL && actor->player->playerstate == PST_DEAD))
+		{
+			break;
+		}
 		actor->health = value;
 		if (actor->player != NULL)
 		{
 			actor->player->health = value;
+		}
+		// If the health is set to a non-positive value, properly kill the actor.
+		if (value <= 0)
+		{
+			actor->Die(activator, activator);
 		}
 		break;
 
@@ -5285,8 +5303,11 @@ int DLevelScript::RunScript ()
 			break;
 
 		case PCD_CLEARLINESPECIAL:
-			if (activationline)
+			if (activationline != NULL)
+			{
 				activationline->special = 0;
+				DPrintf("Cleared line special on line %d\n", activationline - lines);
+			}
 			break;
 
 		case PCD_CASEGOTO:
@@ -5923,6 +5944,8 @@ int DLevelScript::RunScript ()
 					line->args[2] = STACK(3);
 					line->args[3] = STACK(2);
 					line->args[4] = STACK(1);
+					DPrintf("Set special on line %d (id %d) to %d(%d,%d,%d,%d,%d)\n",
+						linenum, STACK(7), specnum, arg0, STACK(4), STACK(3), STACK(2), STACK(1));
 				}
 				sp -= 7;
 			}
