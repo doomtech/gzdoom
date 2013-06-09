239c46ee27ff996d7aff86a617af3186986140ee
graf <graf@b0f79afe-0144-0410-b225-9a4edf0717df>
2013-06-09 07:36:25 +0000

    - bump version number.
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1597 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/version.h
+++ src/version.h
@@ -41,17 +41,17 @@
 
 /** Lots of different version numbers **/
 
-#define DOTVERSIONSTR_NOREV "1.7.1"
+#define DOTVERSIONSTR_NOREV "1.8.0"
 #define ZDVER_STRING "2.7.0"
 
 // The version string the user actually sees.
 #define DOTVERSIONSTR DOTVERSIONSTR_NOREV " (r" SVN_REVISION_STRING ") / ZDoom " ZDVER_STRING " (r" ZD_SVN_REVISION_STRING ")"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 1,7,1,SVN_REVISION_NUMBER
-#define RC_PRODUCTVERSION 1,7,1,0
+#define RC_FILEVERSION 1,8,0,SVN_REVISION_NUMBER
+#define RC_PRODUCTVERSION 1,8,0,0
 #define RC_FILEVERSION2 DOTVERSIONSTR
-#define RC_PRODUCTVERSION2 "1.7"
+#define RC_PRODUCTVERSION2 "1.8"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
